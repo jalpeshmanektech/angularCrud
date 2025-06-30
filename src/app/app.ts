f336f41fddb317c83f3a5ff2d48a3e17a91bcccd
jalpeshmanektech <jalpeshnakummt@gmail.com>
2025-06-30 12:07:41 +0530

    changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./login/login";
+import { Signup } from "./signup/signup";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login, Signup],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'angularCrud';
+  name = "Jalpesh";
+  x = 10;
+  y =20;
 }
