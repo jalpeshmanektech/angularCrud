73b13f8fc447343e892dc1adce375e3c34d14ee0
jalpeshmanektech <jalpeshnakummt@gmail.com>
2025-06-30 12:30:08 +0530

    modified custom component

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -2,8 +2,8 @@ import { Component } from "@angular/core";
 
 @Component ({
     selector:'app-profile',
-    template:'<h1>Hello from Profile</h1>',
-    styles: 'h1{color:green}'
+    templateUrl: './profile.html',
+    styleUrl:'./profile.css',
 })
 
 export class Profile{
