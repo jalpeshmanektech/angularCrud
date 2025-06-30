24d80962346987a8a36508bfb5fff4b43993c207
jalpeshmanektech <jalpeshnakummt@gmail.com>
2025-06-30 12:24:30 +0530

    added custom component

---
+++ src/app/profile/profile.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector:'app-profile',
+    template:'<h1>Hello from Profile</h1>',
+    styles: 'h1{color:green}'
+})
+
+export class Profile{
+
+}
